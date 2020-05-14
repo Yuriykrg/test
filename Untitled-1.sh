7f111ecc2447447877e41fd1fa88386417229692
Yuriy <y.alexandrov@gkamega.com>
2020-05-14 12:04:50 +0600

    test

---
+++ Untitled-1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt update
+apt upgrade
+apt install nginx 
+apt install php-fpm
+apt install php7.2-cli php7.2-curl php7.2-gd php7.2-mysql php7.2-mbstring zip unzip php7.2-xml php7.2-zip
+apt install proftpd
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.timeweb.ru/mariadb/repo/10.3/ubuntu bionic main'
+apt install mariadb-server
+mysql_secure_installation
+add-apt-repository ppa:certbot/certbot
+apt install python-certbot-nginx
