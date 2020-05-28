5442bed85a0f10ce541411bf3cc325437bc78997
Yuriy <grafmontekristo131@gmail.com>
2020-05-28 10:22:39 +0600

    besit linux

--- Untitled-1.sh
+++ Untitled-1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
 apt upgrade
-apt install nginx php-fpm php7.2-cli php7.2-curl php7.2-gd php7.2-mysql php7.2-mbstring zip unzip php7.2-xml php7.2-zip proftpd -y
+apt install nginx proftpd php-fpm php7.2-cli php7.2-curl php7.2-gd php7.2-mysql php7.2-mbstring zip unzip php7.2-xml php7.2-zip  -y
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.timeweb.ru/mariadb/repo/10.3/ubuntu bionic main'
 apt install mariadb-server
